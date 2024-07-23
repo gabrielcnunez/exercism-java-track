4defdde813777878803aa19d27895549cfd92445
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-07-23 10:11:00 -0700

    Solve lasagna exercise

--- lasagna/src/main/java/Lasagna.java
+++ lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,17 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven() {
+        return 40;
+    }
 
-    // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven(int ovenTime) {
+        return expectedMinutesInOven() - ovenTime;
+    }
 
-    // TODO: define the 'preparationTimeInMinutes()' method
+    public int preparationTimeInMinutes(int layers) {
+        return 2 * layers;
+    }
 
-    // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes(int layers, int ovenTime) {
+        return ovenTime + preparationTimeInMinutes(layers);
+    }
 }
