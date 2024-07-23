f9e422a459548bd710cea154aa6343036b26a593
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-07-23 10:19:48 -0700

    Download log levels exercise

---
+++ log-levels/src/main/java/LogLevels.java
@@ -0,0 +1,14 @@
+public class LogLevels {
+    
+    public static String message(String logLine) {
+        throw new UnsupportedOperationException("Please implement the (static) LogLevels.message() method");
+    }
+
+    public static String logLevel(String logLine) {
+        throw new UnsupportedOperationException("Please implement the (static) LogLevels.logLevel() method");
+    }
+
+    public static String reformat(String logLine) {
+        throw new UnsupportedOperationException("Please implement the (static) LogLevels.reformat() method");
+    }
+}
