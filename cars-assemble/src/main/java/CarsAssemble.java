2030347de668c837e0076ca1aa6f7dafd2edf80b
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-07-29 10:17:47 -0700

    Solve cars assemble exercise

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,22 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+        int carsPerHour = 221;
+        if (speed == 10) {
+            return .77 * speed * carsPerHour;
+        } else if (speed == 9) {
+            return .8 * speed * carsPerHour;
+        } else if (speed >= 5 && speed <= 8) {
+            return .9 * speed * carsPerHour;
+        } else if (speed >= 1 && speed <= 4) {
+            return speed * carsPerHour;
+        } else {
+            return 0;
+        }
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        int roundedRate = (int)Math.round(productionRatePerHour(speed));
+        return roundedRate / 60;
     }
 }
